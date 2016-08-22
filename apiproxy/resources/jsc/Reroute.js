2e4ba7ab8e1a32a8d5582cebd646c2dafca53394
Sarthak Gangopadhyay <sarthak@apigee.com>
2016-08-22 12:42:54 -0400

    Corrected camel casing errors

--- apiproxy/resources/jsc/Reroute.js
+++ apiproxy/resources/jsc/Reroute.js
@@ -1,7 +1,7 @@
 var baseUrl='http://dev-int-kube.aeg.cloud/api/v1/proxy/namespaces/sandbox-tony/services/augustseventeenl/'
 
-var version=context.getVariable("uriRequest.majorVersion");
+var version=context.getVariable("urirequest.majorVersion");
 if(version!=null)
-context.setVariable("target.url", baseUrl+context.getVariable("uriRequest.majorVersion"));
+context.setVariable("target.url", baseUrl+context.getVariable("urirequest.majorVersion"));
 else
 context.setVariable("target.url", baseUrl);
